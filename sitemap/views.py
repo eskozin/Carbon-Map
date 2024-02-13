828a8819e58160a1331b3c36bc468e9b9d11a93c
eskozin <68194967+eskozin@users.noreply.github.com>
2024-02-13 16:27:29 +0500

    Add files via upload
    Add excel file with CDE. Add path to excel file.

--- sitemap/views.py
+++ sitemap/views.py
@@ -5,6 +5,7 @@ from folium.plugins import HeatMap
 from shapely.geometry import Polygon
 import pandas as pd
 import numpy as np
+import os
 
 # Create your views here.
 def index(request):
@@ -14,7 +15,8 @@ def map(request):
     m = folium.Map(location=[57.1522, 65.5272],
                    zoom_start=10)
     folium.Marker(location=[57.1522, 65.5272], tooltip='click for more', popup='Tyumen').add_to(m)
-    df = pd.read_excel(r'D:\Работа\Наука\Карбоновые единицы\рассчеты по числу полос 13.02.24.xlsx', sheet_name='Лист2')
+
+    df = pd.read_excel(os.path.join(os.getcwd(),'CDE.xlsx'), sheet_name='Лист2')
     for i in range(1, 11):
         HeatMap(df[['Широта', 'Долгота', i]].to_numpy(), name=f'{i} балл').add_to(m)
     m.add_child(folium.LayerControl(collapsed=False))
